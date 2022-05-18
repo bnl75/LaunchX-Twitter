5ed61d915402a70527c447f52040009b64c82682
bnl75 <diegoartu@hotmail.com>
2022-05-18 01:26:28 -0500

    Requerimiento 3 y 4

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -14,4 +14,25 @@ describe ("Unit Tests for User class", () => {
         expect(user.dateCreated).not.toBeUndefined(); //Verifica que el valor no sea undefined
         expect(user.lastUpdated).not.toBeUndefined();
     });
+
+
+
+    test ('Add getters', () => {
+        const user = new User(1, "bnl75", "Diego", "Bio");
+        expect (user.getUsername).toBe("bnl75");
+        expect (user.getBio).toBe("Bio");
+        expect (user.getDateCreated).not.toBeUndefined();
+        expect (user.getLastUpdated).not.toBeUndefined();
+    });
+
+
+
+    test('Add setters', () => {
+        const user = new User(1, "bnl75", "Diego", "Bio");
+        user.setUsername = "Bernal";
+        expect(user.username).toBe("Bernal");
+
+        user.setBio = "New bio";
+        expect(user.bio).toBe("New bio");
+    });
 });
